f692bccae1d07aff948a00b4a0d4be7d4448dbd5
Hayk-Grigoryan-92 <hayk.grigoryan992@gmail.com>
2023-09-19 18:37:08 +0400

    Create Navbar Component

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ class App extends React.Component{
   render(){
     return (
       <div className="App">
-        <Modal/>
-        {/* <Navbar/> */}
+        {/* <Modal/> */}
+        <Navbar/>
       </div>
     );
   }
